dba0de4c2b3b13cbc598bd479eeb90265ed3be7b
aliterliksiz <aliterliksiz46@gmail.com>
2022-03-14 00:18:07 +0300

    Add project files.

---
+++ FootballApplication/FootballContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApplication
+{
+    public class FootballContext:DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<PlayerAddress> Addresses { get; set; }
+        public FootballContext():base("FootballDbConStr")
+        {
+
+        }
+    }
+}
---
+++ FootballApplication/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApplication
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short FoundationYear { get; set; }
+        public ICollection<Player> Players { get; set; }
+
+        public ICollection<Sponsor> Sponsors { get; set; }
+    }
+}
