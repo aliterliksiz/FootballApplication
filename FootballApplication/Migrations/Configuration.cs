7a5387a54bf6511f1e2152bc91efb6a033b2b3cd
aliterliksiz <aliterliksiz46@gmail.com>
2022-03-14 17:39:51 +0300

    Last Changes

--- FootballApplication/Migrations/Configuration.cs
+++ FootballApplication/Migrations/Configuration.cs
@@ -14,6 +14,13 @@
 
         protected override void Seed(FootballApplication.FootballContext context)
         {
+            context.Teams.AddOrUpdate
+                (
+                new Team { Id = 1, Name = "Galatasaray", FoundationYear = 1905},
+                new Team { Id = 2, Name = "Fenerbahçe", FoundationYear = 1907 },
+                new Team { Id = 3, Name = "Beşiktaş", FoundationYear = 1903 }
+                );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
